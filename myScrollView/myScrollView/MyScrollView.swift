fc4723917d163fbb2168e1747fdfe3317bc2feb4
Achia Landry <lansachia@icloud.com>
2018-06-11 09:46:23 -0700

    "created MyScrollView class"

---
+++ myScrollView/myScrollView/MyScrollView.swift
@@ -0,0 +1,21 @@
+//
+//  MyScrollView.swift
+//  myScrollView
+//
+//  Created by Landry Achia Ndong on 2018-06-11.
+//  Copyright © 2018 Landry Achia Ndong. All rights reserved.
+//
+
+import UIKit
+
+class MyScrollView: UIView {
+
+    var contentSize: CGSize
+    
+    var panGesture = UIPanGestureRecognizer()
+    
+    
+    
+    
+
+}
---
+++ myScrollView/myScrollView/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  myScrollView
+//
+//  Created by Landry Achia Ndong on 2018-06-07.
+//  Copyright © 2018 Landry Achia Ndong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        let mainView = self.view
+//        
+//        
+//        let redFrame = CGRect(x:20, y:20, width:100, height: 100)
+//        let redFrameView = UIView(frame:redFrame)
+//        redFrameView.backgroundColor = UIColor.red
+//        mainView?.addSubview(redFrameView)
+//        
+//        //green frame
+//        let greenFrame = CGRect(x:150, y:150, width:150, height: 200)
+//        let greenFrameView = UIView(frame:greenFrame)
+//        greenFrameView.backgroundColor = UIColor.blue
+//        mainView?.addSubview(greenFrameView)
+//        
+//        //blue frame
+//        let blueFrame = CGRect(x:40, y:400, width:200, height: 150)
+//        let blueFrameView = UIView(frame:blueFrame)
+//        blueFrameView.backgroundColor = UIColor.blue
+//        mainView?.addSubview(blueFrameView)
+//        
+//        //yellow frame
+//        let yellowFrame = CGRect(x:100, y:600, width:180, height: 150)
+//        let yellowFrameView = UIView(frame:yellowFrame)
+//        yellowFrameView.backgroundColor = UIColor.blue
+//        mainView?.addSubview(yellowFrameView)
+    }
+
+}
+
