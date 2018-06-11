d5756101d4ed794e2f38e7ed48576af90c050963
Achia Landry <lansachia@icloud.com>
2018-06-11 16:57:46 -0700

    "fixing scrollViewController"

--- myScrollView/myScrollView/MyScrollView.swift
+++ myScrollView/myScrollView/MyScrollView.swift
@@ -8,16 +8,49 @@
 
 import UIKit
 
+
+
 class MyScrollView: UIView {
 
-//    var contentSize: CGSize
-//
-//    var panGesture = UIPanGestureRecognizer()
-//
-//    func handlePan(gesture: UIPanGestureRecognizer) {
-//
-//    }
+    var contentSize: CGSize?
+    var panGesture = UIPanGestureRecognizer()
+    
+    var initialCenter = CGPoint()  // The initial center point of the view.
+    
+    let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(panPiece(_:)))
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.isUserInteractionEnabled = true
+        self.addGestureRecognizer(panGestureRecognizer)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // Here we use the method didPan(sender:), which we defined in the previous step, as the action.
     
     
-
+    @objc func panPiece(_ gestureRecognizer : UIPanGestureRecognizer) {
+        guard gestureRecognizer.view != nil else {return}
+        let piece = gestureRecognizer.view!
+        // Get the changes in the X and Y directions relative to
+        // the superview's coordinate space.
+        let translation = gestureRecognizer.translation(in: piece.superview)
+        if gestureRecognizer.state == .began {
+            // Save the view's original position.
+            self.initialCenter = piece.center
+        }
+        // Update the position for the .began, .changed, and .ended states
+        if gestureRecognizer.state != .cancelled {
+            // Add the X and Y translation to the view's original position.
+            let newCenter = CGPoint(x: initialCenter.x + translation.x, y: initialCenter.y + translation.y)
+            piece.center = newCenter
+        }
+        else {
+            // On cancellation, return the piece to its original location.
+            piece.center = initialCenter
+        }
+    }
 }
--- myScrollView/myScrollView/ViewController.swift
+++ myScrollView/myScrollView/ViewController.swift
@@ -8,39 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController{
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let baseView = MyScrollView(frame: self.view.bounds)
+        self.view.addSubview(baseView)
+        
+        
         let screenSize: CGRect = UIScreen.main.bounds
-        let mainView = UIView(frame: CGRect(x: 0, y: -100, width: screenSize.width, height: screenSize.height))
-        print("\(screenSize.height), \(screenSize.width)")
-        self.view.addSubview(mainView)
+//        let mainView = UIView(frame: CGRect(x: 0, y: 0, width: screenSize.width, height: screenSize.height))
+////        let mainView = baseView
+//        print("\(screenSize.height), \(screenSize.width)")
+//        baseView.addSubview(mainView)
         
         
         let redFrame = CGRect(x:20, y:20, width:100, height: 100)
         let redFrameView = UIView(frame:redFrame)
         redFrameView.backgroundColor = UIColor.red
-        mainView.addSubview(redFrameView)
+        baseView.addSubview(redFrameView)
         
         //green frame
         let greenFrame = CGRect(x:150, y:150, width:150, height: 200)
         let greenFrameView = UIView(frame:greenFrame)
-        greenFrameView.backgroundColor = UIColor.blue
-        mainView.addSubview(greenFrameView)
+        greenFrameView.backgroundColor = UIColor.green
+        baseView.addSubview(greenFrameView)
         
         //blue frame
         let blueFrame = CGRect(x:40, y:400, width:200, height: 150)
         let blueFrameView = UIView(frame:blueFrame)
-        blueFrameView.backgroundColor = UIColor.yellow
-        mainView.addSubview(blueFrameView)
+        blueFrameView.backgroundColor = UIColor.blue
+        baseView.addSubview(blueFrameView)
         
         //yellow frame
         let yellowFrame = CGRect(x:100, y:600, width:180, height: 150)
         let yellowFrameView = UIView(frame:yellowFrame)
-        yellowFrameView.backgroundColor = UIColor.blue
-        mainView.addSubview(yellowFrameView)
+        yellowFrameView.backgroundColor = UIColor.yellow
+        baseView.addSubview(yellowFrameView)
+        
+        
+       
+        
+    
+        
     }
 
 }
